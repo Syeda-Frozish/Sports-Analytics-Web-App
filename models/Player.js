d1424d8987f1849c12043c04a1cada5375ff5c1c
Syeda-Frozish <frozishkazmi24@gmail.com>
2026-05-09 00:38:05 +0500

    Players Model

---
+++ models/Player.js
@@ -0,0 +1,89 @@
+// models/Player.js
+
+const mongoose = require("mongoose");
+
+const playerSchema = new mongoose.Schema(
+  {
+    playerId: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+
+    name: {
+      first: {
+        type: String,
+        required: true,
+      },
+
+      last: {
+        type: String,
+        required: true,
+      },
+
+      full: {
+        type: String,
+        required: true,
+      },
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+
+    dateOfBirth: {
+      type: String,
+      required: true,
+    },
+
+    gender: {
+      type: String,
+      required: true,
+    },
+
+    battingStyle: {
+      type: String,
+      required: true,
+    },
+
+    bowlingStyle: {
+      type: String,
+      default: null,
+    },
+
+    position: {
+      type: String,
+      required: true,
+    },
+
+    country: {
+      id: {
+        type: Number,
+        required: true,
+      },
+
+      name: {
+        type: String,
+        required: true,
+      },
+
+      continent: {
+        type: String,
+        required: true,
+      },
+
+      image: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Player = mongoose.model("Player", playerSchema);
+
+module.exports = Player;
