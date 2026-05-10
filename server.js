476c0d12b3625ba22a830b550b600c385f5a6abd
M-Kaif-Malik <m.kaifmalik9@hotmail.com>
2026-05-11 01:43:27 +0500

    deployment

--- server.js
+++ server.js
@@ -44,6 +44,10 @@ app.get('/', (req, res) => {
   res.send('Sports Analytics API is running...');
 });
 
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
